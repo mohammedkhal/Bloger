8d1ea2453c676a341e5ee3436411fb6fb29afa7a
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-08 13:49:46 +0300

    Update CategoryJoin.php
    make  relationships

--- app/Models/CategoryJoin.php
+++ app/Models/CategoryJoin.php
@@ -29,7 +29,14 @@ class CategoryJoin extends Model
         'updated_at',
     ];
 
-  
-
+    public function post()
+    {
+        return $this->belongsTo('App\Models\Post', 'post_id' , 'id');
+    }
+    
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category', 'category_id' , 'id');
+    }
  
 }
