e11af771a1e4a6c5ceeadb011b19effe89728d32
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-08 17:57:21 +0300

    Update 2014_10_12_100000_create_password_resets_table.php
    change the data typr from int to bigInteger

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,9 +14,10 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('email')->index();
-            $table->integer('user_id');
-            $table->foreign('user_id')->reference('id')->on('users');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');  
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
