fe5b5b2ab3958736d91b1c76a3870bb0b0d9962a
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-08 18:00:02 +0300

    Update 2019_04_07_151059_create_update_post_reveiws_table.php
    make time stamp nullable

--- database/migrations/2019_04_07_151059_create_update_post_reveiws_table.php
+++ database/migrations/2019_04_07_151059_create_update_post_reveiws_table.php
@@ -15,16 +15,16 @@ class CreateUpdatePostReveiwsTable extends Migration
     {
         Schema::create('update_post_reveiws', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('post_review_id')->unsigned();
+            $table->bigInteger('post_review_id')->unsigned();
             $table->foreign('post_review_id')->references('id')->on('post_reveiws');
-            $table->integer('admin_id')->unsigned();
+            $table->bigInteger('admin_id')->unsigned();
             $table->foreign('admin_id')->references('id')->on('admins');
-            $table->integer('user_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->boolean('status')->default(true);
             $table->string('comment');
-            $table->timestamp('accept_date');
-            $table->timestamp('refuse_date');
+            $table->timestamp('accept_date')->nullable();
+            $table->timestamp('refuse_date')->nullable();
             
         });
     }
