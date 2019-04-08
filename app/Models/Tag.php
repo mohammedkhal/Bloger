73b6df9de190b48ebe94cb072aff905cc398f89b
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-08 13:52:40 +0300

    new relationships

---
+++ app/Models/Tag.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Tag extends Model
+{
+    use Notifiable;
+
+    public $table="tags" ;
+
+    protected $casts = [
+        'id' => 'integer',
+        'tag_name' => 'string',
+        'slug' => 'string',
+        'number_of_post' => 'integer',
+        'creation_date' => 'date',
+        'last_use' => 'date',
+
+    ];
+
+
+    protected $fillable = [
+        'id', 'tag_name' , 'slug', 'number_of_post' , 'creation_date' , 'last_use' ,
+    ];
+
+    
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function tagJoin()
+    {
+        return $this->hasMany('App\Models\TagJoin', 'tag_id' , 'id');
+    }
+  
+
+ 
+}
