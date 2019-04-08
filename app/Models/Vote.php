fad61ac04ccc449471cc55889f951538a4020ea0
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-08 13:54:58 +0300

    new relationships

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -24,12 +24,21 @@ class Vote extends Model
         'id','user_id','vote'
     ];
 
-
-
     protected $dates = [
         'created_at',
         'updated_at',
     ];
+
+    public function post()
+    {
+        return $this->belongsTo('App\Models\Post', 'post_id' , 'id');
+    }
+
+    
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id' , 'id');
+    }
 
 
 
