bb8c064f88022b5926cf8e3ed0b8a567bf62902c
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-08 18:01:42 +0300

    Update 2019_04_07_151513_create_votes_table.php
    conver to bigInteger

--- database/migrations/2019_04_07_151513_create_votes_table.php
+++ database/migrations/2019_04_07_151513_create_votes_table.php
@@ -15,7 +15,7 @@ class CreateVotesTable extends Migration
     {
         Schema::create('votes', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('user_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->integer('vote')->default(0);
             $table->timestamps();
