4e95c2e76ec697b000e033516ca71ce8b42f5529
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-09 11:34:49 +0300

    change the bigInteger type to ineger

--- app/Models/SinginOperationAdmin.php
+++ app/Models/SinginOperationAdmin.php
@@ -14,27 +14,26 @@ class SinginOperationAdmin extends Model
     public $table="singin_operation_admins" ;
 
     protected $casts = [
-        'id' =>'BigInteger' ,
-        'admin_id' =>'BigInteger',
+        'id' =>'integer' ,
+        'admin_id' =>'integer',
         'ip' =>'string',
         'country' =>'string',
         'device_type' =>'string',
         'browser' =>'string',
         'operating_system' =>'string',
-        'login_date' =>'date',
-        'last_login' =>'date',
-        'session_end_date' =>'date',
+    
        ];
 
     protected $fillable = [
         'id', 'admin_id' , 'ip', 'country', 'device_type' ,
-        'browser' , 'operating_system' , 'login_date' , 'last_login' , 'session_end_date' 
+        'browser' , 'operating_system' , 'last_signin' , 'signin_at' , 'session_end_at' 
     ];
 
     
     protected $dates = [
-        'created_at',
-        'updated_at',
+        'last_signin',
+        'signin_at',
+        'session_end_at',
     ];
 
   
