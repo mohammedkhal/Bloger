3cb31f8440e195a228a55ea53cd2f6a0dba8e3c6
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-09 11:37:33 +0300

    add "d" to created

--- database/migrations/2019_04_07_151218_create_tags_table.php
+++ database/migrations/2019_04_07_151218_create_tags_table.php
@@ -18,7 +18,7 @@ class CreateTagsTable extends Migration
             $table->string('tag_name');
             $table->string('slug');
             $table->integer('number_of_posts')->default(0);
-            $table->timestamp('create_at')->nullable();
+            $table->timestamp('created_at')->nullable();
             $table->timestamp('last_use')->nullable();
         });
     }
