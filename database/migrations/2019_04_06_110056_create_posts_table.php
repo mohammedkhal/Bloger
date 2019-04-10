3f17f2657fb80ca1420e8ab9f4dc137640073aba
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-10 09:24:01 +0300

    adding new coulm to posts table

--- database/migrations/2019_04_06_110056_create_posts_table.php
+++ database/migrations/2019_04_06_110056_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('title');
             $table->string('slug');
             $table->string('short_description');
