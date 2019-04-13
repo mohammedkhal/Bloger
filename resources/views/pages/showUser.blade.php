c13f46bab8f35d8f2e8d84b94dc1391f595ee72e
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-13 17:09:41 +0300

    chang the route name

--- resources/views/pages/showUser.blade.php
+++ resources/views/pages/showUser.blade.php
@@ -3,7 +3,12 @@
 
 @section('content')
 
+
 @foreach ($users as $user)
-   <a href="{{route('account.show' , ['username' => $user->username])}}"> <h5>{{$user->first_name  }}  {{$user->third_name}}</h5> </a> <br>
+
+<a href="{{route('user.find' , ['slug' => $user->username])}}"> <h5>{{$user->first_name  }}  {{$user->third_name}}</h5> </a> 
+   @if ($user->id == auth('user')->id() )
+     <a href="{{ route('account.show' , ['slug' => $user->username]) }}"> <button class="btn btn-primary"> edite ur Personal data</button> </a>       
+   @endif 
 @endforeach
 @endsection
