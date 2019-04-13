d5b5707a1899e02cd7a270000525a136c05ca834
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-13 11:05:55 +0300

    changs from articles to post

--- routes/web.php
+++ routes/web.php
@@ -17,47 +17,47 @@ Route::get('/', function () {
 });
 
 
-Route::group(['prefix' => 'articles', 'namespace' => 'Articles'], function () {
-    Route::get('/', 'ArticlesController@index')->name('articles.index');
+Route::group(['prefix' => 'posts', 'namespace' => 'Posts'], function () {
+    Route::get('/', 'PostController@index')->name('posts.index');
     
-    Route::get('/create', 'CreateController@show')->name('articles.create');
-    Route::post('/create', 'CreateController@store')->name('articles.create.store');
+    Route::get('/create', 'CreateController@show')->name('posts.create');
+    Route::post('/create', 'CreateController@store')->name('posts.create.store');
     
-    Route::get('/{slug}', 'ArticlesController@show')->name('articles.article.show');
+    Route::get('/{slug}', 'PostController@show')->name('posts.post.show');
 
-    Route::get('/{slug}/edit', 'EditController@show')->name('articles.article.edit');
-    Route::post('/{slug}/edit', 'EditController@update')->name('articles.article.edit.update');
+    Route::get('/{slug}/edit', 'EditController@find')->name('posts.post.edit');
+    Route::post('/{slug}/edit', 'EditController@update')->name('posts.post.edit.update');
 
-    Route::post('/{slug}/reviews', 'ReviewController@show')->name('articles.article.review.show');
+    Route::post('/{slug}/reviews', 'ReviewController@show')->name('posts.post.review.show');
     
-    Route::post('/{slug}/category', 'CaregoryController@show')->name('articles.article.category.show');
+    Route::post('/{slug}/category', 'CaregoryController@show')->name('posts.post.category.show');
 
-    Route::post('/{slug}/vote', 'VoteController@update')->name('articles.article.vote.update');
+    Route::post('/{slug}/vote', 'VoteController@update')->name('posts.post.vote.update');
 });
 
 
-Route::group(['prefix' => 'auth', 'namespace' => 'Authorization'], function () {
-    Route::get('/sign-up', 'SignUpController@createUser')->name('auth.sign-up');
-    Route::post('/sign-up', 'SignUpController@storeUser')->name('auth.sign-up.store');
+Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
+    Route::get('/sign-up', 'SignUpController@create')->name('auth.sign-up');
+    Route::post('/sign-up', 'SignUpController@store')->name('auth.sign-up.store');
 
-    Route::get('/sign-in', 'SignInController@createLogin')->name('auth.sign-in');
-    Route::post('/sign-in', 'SignInController@storeUser')->name('auth.sign-in.auth');
+    Route::get('/sign-in', 'SignInController@create')->name('auth.sign-in');
+    Route::post('/sign-in', 'SignInController@store')->name('auth.sign-in.auth');
 
-    Route::get('/sign-out', 'SignOutController@destroyUser')->name('auth.sign-out.auth');
+    Route::get('/sign-out', 'SignOutController@signOut')->name('auth.sign-out.auth');
 });
 
 Route::group(['prefix' => 'dashboard', 'namespace' => 'Dashboard'], function () {
     Route::post('/', 'AdminController@dashboard')->name('index');
-    Route::get('auth/sign-in', 'SessionsController@createLogin')->name('signin');
-    Route::post('auth/sign-in', 'SessionsController@storeAdmin')->name('signin.store');
-    Route::get('auth/sign-out', 'SessionsController@destroyUser')->name('signout');
+    Route::get('auth/sign-in', 'SignInController@create')->name('signin');
+    Route::post('auth/sign-in', 'SignInController@store')->name('signin.store');
+    Route::get('auth/sign-out', 'SignInController@signout')->name('signout');
 
 });
 
 Route::group(['prefix' => 'account', 'namespace' => 'Account'], function () {
     Route::get('/{username}', 'EditController@showUser')->name('account.show');
     Route::post('/', 'EditController@updateUser')->name('account.update');
-    Route::get('/', 'UserController@allUser')->name('account.all');
+    Route::get('/', 'UserController@show')->name('account.all');
 
 });
 
