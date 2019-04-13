de020d7dc3832ac6a63616b3fee638f2cca025ba
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-13 17:09:00 +0300

    add user controller

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\User ;
+use App\Services\UserService ;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+
+class UserController extends Controller {
+
+    protected $user;
+
+    public function __construct(UserService $user)
+    {
+        $this->user = $user;
+    }
+    
+    public function index(){
+
+        $users =  $this->user->index() ; 
+        return view('pages.showUser' , compact('users')) ;
+    }
+
+    public function find($slug){
+
+        $user =  $this->user->find($slug) ; 
+        return view('users.showOneUser' , compact('user')) ;
+    }
+      
+
+
+}
