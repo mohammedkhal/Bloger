e3101ebcfd29f7930831ebdc73f46dcf0cf07750
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-14 14:32:46 +0300

    change the controller name

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,17 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'posts', 'namespace' => 'Posts'], function () {
     Route::get('/', 'PostController@index')->name('posts.index');
-    
+
     Route::get('/create', 'CreateController@show')->name('posts.create');
     Route::post('/create', 'CreateController@store')->name('posts.create.store');
-    
+
     Route::get('/{slug}', 'PostController@show')->name('posts.post.show');
 
     Route::get('/{slug}/edit', 'EditController@find')->name('posts.post.edit');
     Route::post('/{slug}/edit', 'EditController@update')->name('posts.post.edit.update');
 
     Route::post('/{slug}/reviews', 'ReviewController@show')->name('posts.post.review.show');
-    
+
     Route::post('/{slug}/category', 'CaregoryController@show')->name('posts.post.category.show');
 
     Route::post('/{slug}/vote', 'VoteController@update')->name('posts.post.vote.update');
@@ -41,7 +41,7 @@ Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
     Route::post('/sign-up', 'SignUpController@store')->name('auth.sign-up.store');
 
     Route::get('/sign-in', 'SignInController@create')->name('auth.sign-in');
-    Route::post('/sign-in', 'SignInController@store')->name('auth.sign-in.auth');
+    Route::post('/sign-in', 'SignInController@auth')->name('auth.sign-in.auth');
 
     Route::get('/sign-out', 'SignOutController@signOut')->name('auth.sign-out.auth');
 });
@@ -49,9 +49,8 @@ Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
 Route::group(['prefix' => 'dashboard', 'namespace' => 'Dashboard'], function () {
     Route::post('/', 'AdminController@dashboard')->name('index');
     Route::get('auth/sign-in', 'SignInController@create')->name('signin');
-    Route::post('auth/sign-in', 'SignInController@store')->name('signin.store');
+    Route::post('auth/sign-in', 'SignInController@auth')->name('signin.store');
     Route::get('auth/sign-out', 'SignInController@signout')->name('signout');
-
 });
 
 Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
@@ -62,6 +61,4 @@ Route::group(['prefix' => 'user', 'namespace' => 'User'], function () {
 Route::group(['prefix' => 'account', 'namespace' => 'Account'], function () {
     Route::post('/{slug}', 'UserEditeController@update')->name('account.update');
     Route::get('/{slug}', 'UserEditeController@edite')->name('account.show');
-
 });
-
