2316f7c1bd7133562d1e1bd51f60af2253c79a31
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-16 08:58:45 +0300

    redirect withe route

--- app/Http/Controllers/Auth/SignOutController.php
+++ app/Http/Controllers/Auth/SignOutController.php
@@ -7,11 +7,16 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Auth;
 class SignOutController extends Controller
 {
+    public function __construct(AuthService $authService)
+    {
+        $this->auth = $authService;
+    }
 
     public function signOut()
     {
+        $this->auth->signout();
         Auth::guard('user')->logout();         
-        return redirect()->back();       
+        return redirect()->route('');       
     }
 
     
