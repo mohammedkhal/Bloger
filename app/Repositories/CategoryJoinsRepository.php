c6f68337f05a9dc041690aaf4a5c7426e46e236f
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-16 12:07:36 +0300

    edit category Reposotiry

--- app/Repositories/CategoryJoinsRepository.php
+++ app/Repositories/CategoryJoinsRepository.php
@@ -1,44 +1,26 @@
 <?php
 
 namespace App\Repositories;
+
 use App\Models\CategoryJoin;
-use Illuminate\Http\Request;
+
 class CategoryJoinsRepository
 {
-
-  protected $category;
-
   public function getModel()
   {
-  return new CategoryJoin ;
+    return new CategoryJoin;
   }
 
-
-  public function store($category, $post_id )
+  public function store($category, $post_id)
   {
-      $categoryObj = $this->getModel() ;
-      $categoryObj->category_id = $category ;
-      $categoryObj->post_id = $post_id ;
-      $categoryObj->save();
-    
-    return $categoryObj ;
+    $categoryJoin = $this->getModel();
+    $categoryJoin->category_id = $category;
+    $categoryJoin->post_id = $post_id;
+    $categoryJoin->save();
+
+    return $categoryJoin;
   }
-  
+
   public function update(array $attributes)
-  {
-    $categoryObj = $this->getModel() ;
-
-    $categoryObj->where('post_id', $attributes['post_id'] )->get()->delete() ;
-     
-    $array =$attributes['category'];
-    foreach($array as $category){
-      $categoryObj = $this->getModel() ;
-      $categoryObj->category_id = $category ;
-      $categoryObj->post_id = $attributes['post_id'] ;
-      $categoryObj->save();
-    }
-    return $categoryObj ;
-  }
-  
-
+  { }
 }
