9f598a62e1ccec484c7043584093b783083ff893
mohammedkhaled <44396391+mohammedkhal@users.noreply.github.com>
2019-04-21 13:03:03 +0300

    uses the data varible come from controller insted of service

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -40,21 +40,21 @@ class UserRepository
     return  $user->where('username', $slug)->first();
   }
 
-  public function update($slug, $attributes)
+  public function update($slug, $data)
   {
     $user = $this->find($slug);
-    $user->first_name =   $attributes['first_name'];
-    $user->second_name =   $attributes['second_name'];
-    $user->third_name =   $attributes['third_name'];
-    $user->website =   $attributes['website'];
-    $user->country =   $attributes['country'];
-    $user->username =   $attributes['username'];
-    if ($attributes['password'] != Null) {
-      $user->password = bcrypt($attributes['password']);
+    $user->first_name =   $data['first_name'];
+    $user->second_name =   $data['second_name'];
+    $user->third_name =   $data['third_name'];
+    $user->website =   $data['website'];
+    $user->country =   $data['country'];
+    $user->username =   $data['username'];
+    if ($data['password'] != Null) {
+      $user->password = bcrypt($data['password']);
     }
-    $user->email =   $attributes['email'];
-    $user->type =   $attributes['type'];
-    $user->profile_pic =  $attributes['profile_pic'];
+    $user->email =   $data['email'];
+    $user->type =   $data['type'];
+    $user->profile_pic =  $data['profile_pic'];
 
     $user->save();
   }
